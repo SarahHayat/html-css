ea69a584437dbf13892bcd4b9be070d4fd6a132c
SarahHayat <saraahyt@gmail.com>
2020-01-09 15:22:14 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit abe779ffd16a119a50cdf44d88f3ce29c8cc4c27.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
-document.querySelector('#choice-1').addEventListener('change', function () {
+document.querySelector('#choice-1').addEventListener('change', function() {
 
     // On stock la valeur du select dans la variable "value"
-    let value = this.value;
+    let value       = this.value;
 
     // On stock l'élément #choix-niveau dans la variable "select"
-    let select = document.querySelector('#level-choice-1');
+    let select      = document.querySelector('#level-choice-1');
 
     // On défini une variable qui passera a vraie quand une selection par défauit sera faite
     let hasSelected = false;
 
 
     // On parcourt toutes les options du select précédemment enregistré
-    select.querySelectorAll('option').forEach(function (item, i) {
+    select.querySelectorAll('option').forEach(function(item, i) {
 
         // On enregistre les classes de l'option courante dans la variable "classList"
         let classList = item.classList;
@@ -19,8 +19,8 @@ document.querySelector('#choice-1').addEventListener('change', function () {
         // SI la variable "value" est inclus dans le tableau des classes "classList"
         // ALORS on retirer la classe "hidden" (on affiche)
         // SINON on ajoute la classe "hidden" (on cache)
-        if (classList.contains(value)) {
-            if (!hasSelected) item.selected = 'selected';
+        if( classList.contains(value) ) {
+            if(!hasSelected) item.selected = 'selected';
             hasSelected = true;
             item.classList.remove('hidden');
         }
@@ -28,20 +28,20 @@ document.querySelector('#choice-1').addEventListener('change', function () {
     });
 
 });
-document.querySelector('#choice-2').addEventListener('change', function () {
+document.querySelector('#choice-2').addEventListener('change', function() {
 
     // On stock la valeur du select dans la variable "value"
-    let value = this.value;
+    let value       = this.value;
 
     // On stock l'élément #choix-niveau dans la variable "select"
-    let select = document.querySelector('#level-choice-2');
+    let select      = document.querySelector('#level-choice-2');
 
     // On défini une variable qui passera a vraie quand une selection par défauit sera faite
     let hasSelected = false;
 
 
     // On parcourt toutes les options du select précédemment enregistré
-    select.querySelectorAll('option').forEach(function (item, i) {
+    select.querySelectorAll('option').forEach(function(item, i) {
 
         // On enregistre les classes de l'option courante dans la variable "classList"
         let classList = item.classList;
@@ -49,8 +49,8 @@ document.querySelector('#choice-2').addEventListener('change', function () {
         // SI la variable "value" est inclus dans le tableau des classes "classList"
         // ALORS on retirer la classe "hidden" (on affiche)
         // SINON on ajoute la classe "hidden" (on cache)
-        if (classList.contains(value)) {
-            if (!hasSelected) item.selected = 'selected';
+        if( classList.contains(value) ) {
+            if(!hasSelected) item.selected = 'selected';
             hasSelected = true;
             item.classList.remove('hidden');
         }
